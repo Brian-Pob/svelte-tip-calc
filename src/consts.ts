6811610b7d9ba8f14984bc81fdccc0309408dda1
Brian-Pob <brianmp.dev@gmail.com>
2024-03-04 21:12:01 -0500

    try to fix error

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,4 @@
-export const URL = 'https://svelte-tip-calc.vercel.app';
+import { dev } from '$app/environment';
+
+export const URL = dev
+  ? 'http://localhost:5173' : 'https://svelte-tip-calc.vercel.app'
